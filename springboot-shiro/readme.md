4da19328b9f77d0505a74f4cdabfdb247fc645b3
HelloWorld521 <563642668@qq.com>
2019-04-12 10:54:25 +0800

    更新 readme

--- springboot-shiro/readme.md
+++ springboot-shiro/readme.md
@@ -11,6 +11,10 @@ springboot + shiro + mybatis-plus 集成项目， 使用 mybatis-plus 生成 ent
 ## 源码
 见本项目
 
+## 项目启动
+1. 导入 sql , 修改数据库配置
+2. [Redis Windows版本](https://github.com/MicrosoftArchive/redis/releases)下载 。直接下载zip版本解压到任意目录即可。 
+   下载后，使用cmd命令切换到Redis根目录，然后运行`redis-server.exe redis.windows.conf`启动即可。
 
 ## 收获
 Please feel free to contact us with any questions you may have.
---
+++ springboot-shiro/src/main/java/com/springboot/demo/sys/controller/SessionController.java
@@ -0,0 +1,41 @@
+package com.springboot.demo.sys.controller;
+
+import com.springboot.demo.common.domain.ResponseBo;
+import com.springboot.demo.sys.entity.UserOnline;
+import com.springboot.demo.sys.service.SessionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/online")
+public class SessionController {
+    @Autowired
+    SessionService sessionService;
+    
+    @RequestMapping("index")
+    public String online() {
+        return "online";
+    }
+
+    @ResponseBody
+    @RequestMapping("list")
+    public List<UserOnline> list() {
+        return sessionService.list();
+    }
+
+    @ResponseBody
+    @RequestMapping("forceLogout")
+    public ResponseBo forceLogout(String id) {
+        try {
+            sessionService.forceLogout(id);
+            return ResponseBo.ok();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseBo.error("踢出用户失败");
+        }
+    }
+}
